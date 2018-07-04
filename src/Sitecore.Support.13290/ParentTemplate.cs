96d01fb71f6dc782dd8fe3c9afbc2aea7bbc16a0
Dmitry Kolinchuk <dmitry.kolinchuk@sitecore.com>
2018-07-04 17:14:15 +0300

    Original code

---
+++ src/Sitecore.Support.13290/ParentTemplate.cs
@@ -0,0 +1,21 @@
+﻿using Sitecore.ContentSearch.ComputedFields;
+using Sitecore.ContentSearch;
+using Sitecore.Data.Items;
+
+namespace Sitecore.XA.Foundation.Search.ComputedFields
+{
+    public class ParentTemplate : AbstractComputedIndexField
+    {
+        public override object ComputeFieldValue(IIndexable indexable)
+        {
+            Item item = indexable as SitecoreIndexableItem;
+
+            if (item?.Parent == null)
+            {
+                return false;
+            }
+
+            return item.Parent.TemplateID;
+        }
+    }
+}
