09b1d0ae52ae7784f3f6911a3775e8acbc899376
DK\obr <olegbraginskyy@sitecore.net>
2018-07-24 10:53:19 +0300

    Init code

---
+++ src/Sitecore.Support.13290/Level.cs
@@ -0,0 +1,21 @@
+﻿using Sitecore.ContentSearch.ComputedFields;
+using Sitecore.ContentSearch;
+using Sitecore.Data.Items;
+
+namespace Sitecore.Support.XA.Foundation.Search.ComputedFields
+{
+  public class Level : AbstractComputedIndexField
+  {
+    public override object ComputeFieldValue(IIndexable indexable)
+    {
+      Item item = indexable as SitecoreIndexableItem;
+
+      if (item?.Parent == null)
+      {
+        return false;
+      }
+
+      return item.Axes.Level;
+    }
+  }
+}
