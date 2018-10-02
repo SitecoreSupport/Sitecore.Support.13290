c051ba62046b7265af171777926dd215f29f1dfe
DK\AMCH <amch@sitecore.net>
2018-10-02 12:27:19 +0300

    commit changes - zero should be returned instead of false in case item is Sitecore root item

--- src/Sitecore.Support.13290/Commerce/XA/Foundation/CommerceEngine/ComputedFields/LevelComputedFieldPatch.cs
+++ src/Sitecore.Support.13290/Commerce/XA/Foundation/CommerceEngine/ComputedFields/LevelComputedFieldPatch.cs
@@ -9,12 +9,28 @@ namespace Sitecore.Support.Commerce.XA.Foundation.CommerceEngine.ComputedFields
     {
         public override object ComputeFieldValue(IIndexable indexable)
         {
-            object obj2 = base.ComputeFieldValue(indexable);
+            #region - modified part to use patched ComputeFieldValue method
+            object obj2 = this.SupportComputeFieldValue(indexable);
+            #endregion
             if ((obj2 != null) && typeof(int).IsAssignableFrom(obj2.GetType()))
             {
                 return obj2;
             }
             return 0;
         }
+
+        #region patched method to return 0 instead of false when item is Sitecore root item
+        public object SupportComputeFieldValue(IIndexable indexable)
+        {
+            Item item = indexable as SitecoreIndexableItem;
+
+            if (item?.Parent == null)
+            {
+                return 0;
+            }
+
+            return item.Axes.Level;
+        }
+        #endregion
     }
 }
