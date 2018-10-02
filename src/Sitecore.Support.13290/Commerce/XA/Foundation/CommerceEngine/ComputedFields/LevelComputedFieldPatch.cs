28b14bb1c85cb0e6daba626ddbd979d7c1a0a399
DK\AMCH <amch@sitecore.net>
2018-10-02 12:23:27 +0300

    commit initial code

---
+++ src/Sitecore.Support.13290/Commerce/XA/Foundation/CommerceEngine/ComputedFields/LevelComputedFieldPatch.cs
@@ -0,0 +1,20 @@
+﻿using Sitecore.ContentSearch;
+using Sitecore.Data.Items;
+using Sitecore.XA.Foundation.Search.ComputedFields;
+using System;
+
+namespace Sitecore.Support.Commerce.XA.Foundation.CommerceEngine.ComputedFields
+{
+    public class LevelComputedFieldPatch : Level
+    {
+        public override object ComputeFieldValue(IIndexable indexable)
+        {
+            object obj2 = base.ComputeFieldValue(indexable);
+            if ((obj2 != null) && typeof(int).IsAssignableFrom(obj2.GetType()))
+            {
+                return obj2;
+            }
+            return 0;
+        }
+    }
+}
